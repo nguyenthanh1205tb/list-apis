d4952d7544d54421ea330ba09c7e555edf39e233
nguyenthanh1205tb <nguyenthanh1205tb@gmail.com>
2021-08-18 13:49:04 +0700

    update ui layout

--- src/layouts/Private.tsx
+++ src/layouts/Private.tsx
@@ -1,14 +1,16 @@
-import React, { PropsWithChildren } from 'react'
+import { Button } from '@blueprintjs/core'
+import React, { PropsWithChildren, Fragment } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
 interface PrivateLayoutProps extends RouteComponentProps {}
 
-function PrivateLayout({ children }: PropsWithChildren<PrivateLayoutProps>) {
+function PrivateLayout({ children, history }: PropsWithChildren<PrivateLayoutProps>) {
   return (
-    <div>
+    <Fragment>
       <h1>PRIVATE LAYOUT</h1>
+      <Button text="Back to login page" onClick={() => history.push('/')} />
       {children}
-    </div>
+    </Fragment>
   )
 }
 
--- src/layouts/Public.tsx
+++ src/layouts/Public.tsx
@@ -1,15 +1,10 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren, Fragment } from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 
 interface PublicLayoutProps extends RouteComponentProps {}
 
 function PublicLayout({ children }: PropsWithChildren<PublicLayoutProps>) {
-  return (
-    <div>
-      <h1>PUBLIC LAYOUT</h1>
-      {children}
-    </div>
-  )
+  return <Fragment>{children}</Fragment>
 }
 
 export default React.memo(PublicLayout)
