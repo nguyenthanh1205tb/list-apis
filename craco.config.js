e6b16ccf4042a115a85074dcfa4df192a6e80bf0
nguyenthanh1205tb <nguyenthanh1205tb@gmail.com>
2021-10-23 15:17:57 +0700

    update

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,21 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
 }
