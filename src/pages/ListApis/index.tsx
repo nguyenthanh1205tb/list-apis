1f9ac21193c881f805faca25ce0d4fd29bdcdeb0
nguyenthanh1205tb <nguyenthanh1205tb@gmail.com>
2021-11-26 19:30:43 +0700

    call

--- src/pages/ListApis/index.tsx
+++ src/pages/ListApis/index.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Collapse } from 'antd'
+import { Request } from 'src/DigestAuth'
 
 const { Panel } = Collapse
 function ListApis() {
@@ -8,18 +9,32 @@ function ListApis() {
     console.log(key)
   }
 
+  const text = `
+    A dog is a type of domesticated animal.
+    Known for its loyalty and faithfulness,
+    it can be found as a welcome guest in many households across the world.
+  `
+
+  useEffect(() => {
+    Request.get({
+      url: '/ISAPI/Event/notification/httpHosts/capabilities',
+    }).then(res => {
+      console.log(res)
+    })
+  }, [])
+
   return (
     <div>
       <Collapse defaultActiveKey={activeKeys} onChange={callback}>
         <Panel header="This is panel header 1" key="1">
-          <p>
-            A dog is a type of domesticated animal. Known for its loyalty and
-            faithfulness, it can be found as a welcome guest in many households
-            across the world.
-          </p>
+          <p>{text}</p>
         </Panel>
-        <Panel header="This is panel header 2" key="2"></Panel>
-        <Panel header="This is panel header 3" key="3"></Panel>
+        <Panel header="This is panel header 2" key="2">
+          <p>{text}</p>
+        </Panel>
+        <Panel header="This is panel header 3" key="3">
+          <p>{text}</p>
+        </Panel>
       </Collapse>
     </div>
   )
